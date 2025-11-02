038ac0bd5304f556014da7dc269e4e66660bd17f
DESKTOP-NH61KMC\ADMIN <ccaotthihhan@gmail.com>
2025-11-03 03:00:36 +0700

    View Home

---
+++ 125CNX_ECommerce/Views/Home/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _125CNX_ECommerce.Views.Home
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
