4eb3604ce9a9e4d8135a7cb651afe99bb9f6441b
DESKTOP-NH61KMC\ADMIN <ccaotthihhan@gmail.com>
2025-11-21 17:45:18 +0700

    config folder git for cao thi han

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace _125CNX_ECommerce.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- Models/UserLoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace _125CNX_ECommerce.Models
-{
-    public class UserLoginModel
-    {
-        public string TenDangNhap { get; set; } = string.Empty;
-        public string MatKhau { get; set; } = string.Empty;
-    }
-}
